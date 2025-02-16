b7968f96d2e61fbd92a32cbb78c0ea6f8aa4f6e3
HuuLoc <huuloc@192.168.x.x.non-exists.ptr.local>
2025-02-17 01:48:51 +0700

    update go

--- main.go
+++ main.go
@@ -8,12 +8,20 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Jake backend"))
 }
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	log.Print("Starting server on :3333")
+	err := http.ListenAndServe(":3333", mux)
 	log.Fatal(err)
 
 }
